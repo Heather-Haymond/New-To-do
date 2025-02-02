98f40a59063ee387090b66296573cf1369931f09
Heather Haymond <hheatherjean11@gmail.com>
2025-02-02 17:50:22 -0600

    server error

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,17 @@
 console.log('JS is sourced!');
+
+//GET -> Read
+function toDo() {
+    // axios call to server
+    axios({
+      method: "GET",
+      url: "/toDo",
+    })
+      .then((response) => {
+        console.log("GET /toDo response.data", response.data);
+        renderToDo(response.data);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  } // end toDo
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
+// GET
+router.get('/', (req, res) => {
+    console.log('GET in /toDo');
+    //sql is similar to axios. it directs info to the DB
+    const sqlText = `
+          SELECT * FROM todos
+          ORDER BY id;
+  `;
+//pool
+pool
+.query(sqlText)
+.then((dbResult) => {
+    let todoTask = dbResult.rows;
+    res.send(todoTask);
+})
+.catch((dbError) => {
+    console.log('Get error in server:', dbError);
+    res.sendStatus(500);    
+   });
+});
 
 
 module.exports = router;
